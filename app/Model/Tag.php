2e75d409cc86a2b8d5cc5c09fef95f66ec46f2ea
eduardo <eduardo.rzeronte@gmail.com>
2016-03-01 21:56:02 +0100

    numbers of tier tags

--- app/Model/Tag.php
+++ app/Model/Tag.php
@@ -23,6 +23,17 @@ class Tag extends Model
         return $this->hasMany('App\Model\TagTranslation');
     }
 
+    public function countScenesLang($language_id)
+    {
+        return Scene::select('scenes.id')
+            ->join('scene_tag', 'scene_tag.scene_id', '=', 'scenes.id')
+            ->join('tags', 'tags.id', '=', 'scene_tag.tag_id')
+            ->join('tag_translations', 'tag_translations.tag_id', '=', 'scene_tag.tag_id')
+            ->where('tag_translations.language_id', '=', $language_id)
+            ->where('tags.id', '=', $this->id)
+            ->count();
+    }
+
     static function getTranslationSearch($query_string = false, $language_id)
     {
         $tags = Tag::select('tags.*', 'tag_translations.name', 'tag_translations.permalink', 'tag_translations.id as translationId')
--- resources/views/_ajax_tagtiers.blade.php
+++ resources/views/_ajax_tagtiers.blade.php
@@ -1,12 +1,17 @@
 <b>Tier1</b>
+
 <br/>
+
 @if (count($tier1) == 0)
     <p>Not tags defined</p>
 @endif
+
 @foreach ($tier1 as $tag)
     <?php $translation = $tag->translations()->where('language_id',$language->id)->first(); ?>
-    <small style="padding:2px;background-color: #3bc53e;color:white;">{{$translation->name}}</small>
+    <small style="padding:2px;background-color: #3bc53e;color:white;">{{$translation->name}} ({{ $tag->countScenesLang($language->id) }})</small>
 @endforeach
+
+<br/>
 
 <b>Tier2</b>
 <br/>
@@ -16,7 +21,7 @@
 
 @foreach ($tier2 as $tag)
     <?php $translation = $tag->translations()->where('language_id',$language->id)->first(); ?>
-    <small style="padding:2px;background-color: #ffa600;color:white;">{{$translation->name}}</small>
+    <small style="padding:2px;background-color: #ffa600;color:white;">{{$translation->name}} ({{ $tag->countScenesLang($language->id) }})</small>
 @endforeach
 
 <b>Tier3</b>
@@ -27,5 +32,5 @@
 
 @foreach ($tier3 as $tag)
     <?php $translation = $tag->translations()->where('language_id',$language->id)->first(); ?>
-    <small style="padding:2px;background-color: #05bdff;color:white;">{{$translation->name}}</small>
+    <small style="padding:2px;background-color: #05bdff;color:white;">{{$translation->name}} ({{ $tag->countScenesLang($language->id) }})</small>
 @endforeach
