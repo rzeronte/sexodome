c22485541b5804ccf641b555aff3c252c2d28ea2
eduardo <eduardo.rzeronte@gmail.com>
2016-06-07 02:08:30 +0200

    ieah

--- resources/views/panel/_head.blade.php
+++ resources/views/panel/_head.blade.php
@@ -1,5 +1,5 @@
 <head>
-    <title>Exporter from Universo</title>
+    <title>Panel v.0.16 | sexodome.com </title>
 
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
