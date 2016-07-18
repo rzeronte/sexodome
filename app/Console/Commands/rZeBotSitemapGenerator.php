9dd4c50bdd5cc2fda12a341b66fcab3cfe4fd4c5
eduardo <eduardo.rzeronte@gmail.com>
2016-07-19 00:15:56 +0200

    sitemap generator for multiples sites

--- app/Console/Commands/rZeBotSitemapGenerator.php
+++ app/Console/Commands/rZeBotSitemapGenerator.php
@@ -2,12 +2,14 @@
 
 namespace App\Console\Commands;
 
+use App\rZeBot\rZeBotUtils;
 use Illuminate\Console\Command;
 use Roumen\Sitemap\Sitemap;
 use Illuminate\Support\Facades\App;
 use App\Model\Language;
 use App\Model\Scene;
 use Request;
+use App\Model\Site;
 
 class rZeBotSitemapGenerator extends Command
 {
@@ -16,14 +18,14 @@ class rZeBotSitemapGenerator extends Command
      *
      * @var string
      */
-    protected $signature = 'rZeBot:sitemap:generate {language_code} {domain}';
+    protected $signature = 'rZeBot:sitemap:generate {site_id}';
 
     /**
      * The console command description.
      *
      * @var string
      */
-    protected $description = 'Generate a new sitemap';
+    protected $description = 'Generate a new sitemap for a site';
 
     /**
      * Execute the console command.
@@ -34,39 +36,53 @@ class rZeBotSitemapGenerator extends Command
     {
         $sitemap = App::make("sitemap");
 
-        $language_code = $this->argument('language_code');
-        $language_id = Language::where('code', $language_code)->first()->id;
 
-        $domain = $this->argument('domain');
+        $site_id = $this->argument('site_id');
+
+        $site = Site::find($site_id);
+
+        if (!$site) {
+            rZeBotUtils::message("El site_id indicado no existe", "red");
+            exit;
+        }
+
+        $language_id = $site->language_id;
 
         $currentHost = Request::server("HTTP_HOST");
 
-        // Scenes
+        // Scenes only for embed feeds
         $scenes = Scene::join('channels', 'channels.id', '=', 'scenes.channel_id')
+            ->select('scenes.*')
             ->where('status', 1)
-            ->where('channels.embed','=', 1)
-            ->limit(10000)
+            ->where('channels.embed', 1)
+            ->limit(40000)
             ->orderBy('published_at', 'desc')
             ->get()
         ;
 
         // Home
-        $sitemap->add(str_replace($currentHost, $domain, route('index', [])), date('Y-m-d').'T00:00:00+00:00', '1.0', 'daily');
+        $sitemap->add(str_replace($currentHost, $site->getHost(), route('index', [])), date('Y-m-d').'T00:00:00+00:00', '1.0', 'daily');
 
         // Categories
-        $sitemap->add(str_replace($currentHost, $domain, route('categories', [])), date('Y-m-d').'T00:00:00+00:00', '1.0', 'daily');
+        $sitemap->add(str_replace($currentHost, $site->getHost(), route('categories', [])), date('Y-m-d').'T00:00:00+00:00', '1.0', 'daily');
 
         // Top Scenes
-        $sitemap->add(str_replace($currentHost, $domain, route('topscenes', [])), date('Y-m-d').'T00:00:00+00:00', '1.0', 'daily');
+        $sitemap->add(str_replace($currentHost, $site->getHost(), route('topscenes', [])), date('Y-m-d').'T00:00:00+00:00', '1.0', 'daily');
 
         // Scenes
+        $i = 0;
         foreach ($scenes as $scene) {
             $translation = $scene->translations()->whereNotNull('permalink')->where('language_id', $language_id)->first();
-            if ($translation) {
-                $sitemap->add(str_replace($currentHost, $domain, route('video', ['permalink'=>$translation->permalink])), date('Y-m-d').'T00:00:00+00:00', '1.0', 'daily');
+
+            if (!$translation) {
+                $this->info("$i - [ERROR] Ignorando URL, la escena " .$scene->id ." no tiene traducción para el idioma id: $language_id");
+            } else {
+                $this->info("$i - Añadiendo al sitemap la url: ".route('video', ['permalink'=>$translation->permalink]));
+                $sitemap->add(str_replace($currentHost, $site->domain, route('video', ['permalink'=>$translation->permalink])), date('Y-m-d').'T00:00:00+00:00', '1.0', 'daily');
             }
+            $i++;
         }
 
-        $sitemap->store('xml', 'sitemap', '');
+        $sitemap->store('xml', $site->getHost(), '');
     }
 }
