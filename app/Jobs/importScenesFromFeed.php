ffc7375c9d0489c81765e27f78a9a4dc588262fc
eduardo <eduardo.rzeronte@gmail.com>
2016-07-19 00:54:48 +0200

    job update

--- app/Jobs/importScenesFromFeed.php
+++ app/Jobs/importScenesFromFeed.php
@@ -37,18 +37,17 @@ class importScenesFromFeed extends Job implements SelfHandling, ShouldQueue
     {
         Log::info('[importScenesFromFeed]');
 
-        echo "feedName: " . $this->queueParams["feed_name"].PHP_EOL;
-        echo "site_id: " . $this->queueParams["site_id"].PHP_EOL;
-        echo "categories: ".PHP_EOL;
-        print_r($this->queueParams["categories"]);
-
-        $exitCode = \Artisan::call('rZeBot:feed:fetch', [
+        $paramsCommand = [
             'feed_name'    => $this->queueParams["feed_name"],
             'site_id'      => $this->queueParams["site_id"],
             '--max'        => ($this->queueParams["max"] != "") ? $this->queueParams["max"]: 'false',
             '--duration'   => ($this->queueParams["duration"] != "") ? $this->queueParams["duration"] : 'false',
             '--categories' => $this->queueParams["categories"],
             '--job'        => $this->queueParams["job"]
-       ]);
+        ];
+        
+        print_r($paramsCommand);
+
+        $exitCode = \Artisan::call('rZeBot:feed:fetch', $paramsCommand);
     }
 }
