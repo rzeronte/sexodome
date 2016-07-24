5b2bbba47554599a7b1ea4c30a887f8fcdda149e
eduardo <eduardo.rzeronte@gmail.com>
2016-07-24 20:10:02 +0200

    translate cmd

--- app/Console/Commands/rZeBotTranslate.php
+++ app/Console/Commands/rZeBotTranslate.php
@@ -14,6 +14,7 @@ use App\Model\Tag;
 use App\Model\Host;
 use App\Model\SceneTag;
 use App\Model\TagClick;
+use App\Model\Site;
 
 class rZeBotTranslate extends Command
 {
@@ -26,9 +27,10 @@ class rZeBotTranslate extends Command
      *
      * @var string
      */
-    protected $signature = 'rZeBot:translate {from} {to}
-                            {--scenes=true : Default options translate scenes}
-                            {--tags=true : Default options translate tags}';
+    protected $signature = 'rZeBot:translate {from} {to} {site_id}
+                            {--scenes=true : Determine if translate scenes}
+                            {--categories=true : Determine if translate categories}
+                            {--tags=true : Determine if translate tags}';
 
     /**
      * The console command description.
@@ -54,13 +56,22 @@ class rZeBotTranslate extends Command
     {
         $from = $this->argument('from');
         $to = $this->argument('to');
+        $site_id = $this->argument('site_id');
 
         $scenes = $this->option('scenes');
         $tags = $this->option('tags');
+        $categories = $this->option('categories');
+
+        $site = Site::find($site_id);
+
+        if (!$site) {
+            rZeBotUtils::message('[ERROR]: El site_id: '. $site_id . " no existe", "red");
+            die();
+        }
 
         if ($scenes == 'true') {
             if ($to != "en") {  // skip override english translations...
-                $this->translateScenes($from, $to);
+                $this->translateScenes($from, $to, $site_id);
             }
         } else {
             echo "[SCENES] jump scenes".PHP_EOL;
@@ -68,19 +79,55 @@ class rZeBotTranslate extends Command
 
         if ($tags == 'true') {
             if ($to != "en") {  // skip override english translations...
-                $this->translateTags($from, $to);
+                $this->translateTags($from, $to, $site_id);
             }
         } else {
             echo "[SCENES] jump tags".PHP_EOL;
         }
+
+        if ($categories == 'true') {
+            if ($to != "en") {  // skip override english translations...
+                $this->translateCategories($from, $to, $site_id);
+            }
+        } else {
+            echo "[SCENES] jump categories".PHP_EOL;
+        }
+
     }
 
-    public function translateScenes($from, $to)
+    public function translateCategories($from, $to, $site_id) {
+        $languageFrom = Language::where('code', $from)->first();
+        $languageTo = Language::where('code', $to)->first();
+
+        $categories = Category::where('site_id', $site_id)->get();
+        $i=0;
+        foreach ($categories as $category) {
+            echo "[ " . number_format(($i*100)/ count($categories), 0) ."% ]";
+            $i++;
+
+            $textFrom = $category->translations()->where('language_id', $languageFrom->id)->first();
+            $translationTo = $category->translations()->where('language_id', $languageTo->id)->first();
+
+            $translationTitle = $this->translateText($textFrom->title, $from, $to);
+
+            if ($translationTitle != false) {
+                $translationTo->title = $translationTitle;
+                $translationTo->permalink = str_slug($translationTitle);
+                $translationTo->save();
+                echo "[CATEGORY] " . $textFrom->title . " - " . $translationTitle.PHP_EOL;
+            } else {
+                echo "[ERROR CATEGORY TRANSLATION] $from <-> $to" . $textFrom->name . " - " . $translationTitle .PHP_EOL;
+            }
+        }
+
+    }
+
+    public function translateScenes($from, $to, $site_id)
     {
         $languageFrom = Language::where('code', $from)->first();
         $languageTo = Language::where('code', $to)->first();
 
-        $scenes = Scene::all();
+        $scenes = Scene::where('site_id', $site_id)->get();
         $i=0;
         foreach ($scenes as $scene) {
             echo "[ " . number_format(($i*100)/ count($scenes), 0) ."% ]";
@@ -89,23 +136,34 @@ class rZeBotTranslate extends Command
             $textFrom = $scene->translations()->where('language_id', $languageFrom->id)->first();
             $translationTo = $scene->translations()->where('language_id', $languageTo->id)->first();
 
+            if (!$textFrom || !$translationTo) {
+                echo "[ERROR SCENE] Can't get from and to objects".PHP_EOL;
+                continue;
+            }
+
+            echo " | Current From: " . $textFrom->title . " ";
+            echo " | Current To: " . $translationTo->title . " ";
             $translationTitle = $this->translateText($textFrom->title, $from, $to);
 
-            $translationTo->title = $translationTitle;
-            $translationTo->permalink = str_slug($translationTitle);
-            $translationTo->save();
-            echo "[SCENE] " . $textFrom->title . " - " . $translationTitle.PHP_EOL;
-
+            if ($translationTitle != false) {
+                $translationTo->title = $translationTitle;
+                $translationTo->permalink = str_slug($translationTitle);
+                $translationTo->save();
+                echo "[SCENE] " . $textFrom->title . " - " . $translationTitle.PHP_EOL;
+            } else {
+                echo "[ERROR SCENES TRANSLATION] $from <-> $to " . $textFrom->name . " - " . $translationTitle .PHP_EOL;
+            }
         }
 
     }
 
-    public function translateTags($from, $to)
+    public function translateTags($from, $to, $site_id)
     {
         $languageFrom = Language::where('code', $from)->first();
         $languageTo = Language::where('code', $to)->first();
 
-        $tags = Tag::all();
+        $tags = Tag::where('site_id', $site_id)->get();
+
         $i=0;
         foreach ($tags as $tag) {
             echo "[ " . number_format(($i*100)/ count($tags), 0) ."% ]";
@@ -116,11 +174,15 @@ class rZeBotTranslate extends Command
 
             $translationName = $this->translateText($textFrom->name, $from, $to);
 
-            $translationTo->name = $translationName;
-            $translationTo->permalink = str_slug($translationName);
-            $translationTo->save();
+            if ($translationName != false) {
+                $translationTo->name = $translationName;
+                $translationTo->permalink = str_slug($translationName);
+                $translationTo->save();
+                echo "[TAG] " . $textFrom->name . " - " . $translationName.PHP_EOL;
+            } else {
+                echo "[ERROR TAG TRANSLATION] " . $textFrom->name . " - " . $translationName.PHP_EOL;
+            }
 
-            echo "[TAG] " . $textFrom->name . " - " . $translationName.PHP_EOL;
         }
     }
 
@@ -141,7 +203,11 @@ class rZeBotTranslate extends Command
         $responseDecoded = json_decode($response, true);
         curl_close($handle);
 
-        $translated = $responseDecoded['data']['translations'][0]['translatedText'];
+        if (isset($responseDecoded['error'])) {
+            $translated = false;
+        } else {
+            $translated = $responseDecoded['data']['translations'][0]['translatedText'];
+        }
 
         return $translated;
     }
