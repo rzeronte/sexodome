ca07f3c369c4538085ba39e3fee1def69fff94aa
eduardo <eduardo.rzeronte@gmail.com>
2016-08-07 04:42:08 +0200

    fix

--- app/Http/Controllers/ConfigController.php
+++ app/Http/Controllers/ConfigController.php
@@ -356,7 +356,7 @@ class ConfigController extends Controller
             }
 
             $categories = Input::get('categories');
-            if (!count($categories)) {
+            if (strlen($categories) == 0) {
                 $categories = 'false';
             }
 
