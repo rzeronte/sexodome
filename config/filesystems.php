2d3cc127232bba9b13c99b3eb4f5fce4934ff519
eduardo <eduardo.rzeronte@gmail.com>
2016-08-09 01:29:42 +0200

    sitemap fix

--- config/filesystems.php
+++ config/filesystems.php
@@ -48,6 +48,11 @@ return [
             'root'   => storage_path('app'),
         ],
 
+        'web' => [
+            'driver' => 'local', 
+            'root'   => public_path(),
+        ],
+
         'ftp' => [
             'driver'   => 'ftp',
             'host'     => 'ftp.example.com',
