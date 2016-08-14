3c38b054b6fd0f5b25fb26e9b3773c485107aef7
eduardo <eduardo.rzeronte@gmail.com>
2016-08-14 23:08:52 +0200

    update template

--- resources/views/panel/_selector_site_option.blade.php
+++ resources/views/panel/_selector_site_option.blade.php
@@ -1,7 +1,9 @@
 <div class='row'>
     <div class='col-md-4'>
-        <span class='label label-primary'><i class='glyphicon glyphicon-th-large'></i> Site: http://{{$s->getHost()}}</span>
-        <span class='label label-success'><i class='glyphicon glyphicon-film'></i> Total scenes: {{$s->getTotalScenes()}}</span>
+        <span class='label label-primary'><i class='glyphicon glyphicon-globe'></i> {{$s->getHost()}}</span>
+        <span class='label label-success'><i class='glyphicon glyphicon-film'></i> Scenes: {{$s->getTotalScenes()}}</span>
+        <span class='label label-success'><i class='glyphicon glyphicon-th-large'></i> Categories: {{$s->categories()->count()}}</span>
         <span class='label label-success'><i class='glyphicon glyphicon-time'></i> CronJobs: {{$s->cronjobs()->count()}}</span>
+        <span class='label label-success'><i class='glyphicon glyphicon-star-empty'></i> Pornstars: {{$s->pornstars()->count()}}</span>
     </div>
 </div>
