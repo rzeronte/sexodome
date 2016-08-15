426010f807debc24f20c37433105d79cc95dab24
eduardo <eduardo.rzeronte@gmail.com>
2016-08-15 06:39:44 +0200

    iframe fix

--- resources/views/tube/_iframe_network.blade.php
+++ resources/views/tube/_iframe_network.blade.php
@@ -1,6 +1,3 @@
 @if (env("SHOW_IFRAME_NETWORK", true))
-    <?php $siteIframe = App\Model\Site::find($site->iframe_site_id) ?>
-    @if ($siteIframe)
-        <iframe src="http://{{$siteIframe->getHost()}}/ads" width="100%" style="border: none;height:205px;overflow:hidden !important;"></iframe>
-    @endif
+    <iframe src="http://{{$site->getHost()}}/ads" width="100%" style="border: none;height:205px;overflow:hidden !important;"></iframe>
 @endif
