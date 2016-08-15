86da6a9e26f01e836fab441650e010da67d8c2d5
eduardo <eduardo.rzeronte@gmail.com>
2016-08-15 03:31:27 +0200

    minor

--- resources/views/panel/_selector_site_option.blade.php
+++ resources/views/panel/_selector_site_option.blade.php
@@ -3,7 +3,7 @@
     <span class='label label-warning'><i class='glyphicon glyphicon-globe'></i> {{$s->getHost()}}</span>
     <small>
         <span class='label label-success'><i class='glyphicon glyphicon-film'></i> Scenes: {{$s->getTotalScenes()}}</span>
-        <span class='label label-success'><i class='glyphicon glyphicon-th-large'></i> Categories: {{$s->categories()->count()}}</span>
+        <span class='label label-success'><i class='glyphicon glyphicon-th-large'></i> Categories: {{$s->categories()->where('status', 1)->count()}}</span>
         <span class='label label-success'><i class='glyphicon glyphicon-time'></i> CronJobs: {{$s->cronjobs()->count()}}</span>
         <span class='label label-success'><i class='glyphicon glyphicon-star-empty'></i> Pornstars: {{$s->pornstars()->count()}}</span>
     </small>
