f37807998c783dcffdd1480e40be78343066655d
eduardo <eduardo.rzeronte@gmail.com>
2016-08-21 22:29:15 +0200

    fix

--- public/admin.js
+++ public/admin.js
@@ -316,7 +316,7 @@ $( document ).ready(function() {
 
     $( "#selector_site" ).change(function() {
         var option = $(this).find('option:selected', this).attr('data-action');
-        if (option != "") {
+        if (option.length > 0) {
             window.location = option;
         }
     });
