b98a2b2cd9ef2ab04f89d60776c296c458c516b6
eduardo <eduardo.rzeronte@gmail.com>
2016-08-23 01:41:29 +0200

    iframe not in mobile

--- resources/views/tube/commons/_iframe_network.blade.php
+++ resources/views/tube/commons/_iframe_network.blade.php
@@ -1,3 +1,9 @@
 @if (env("SHOW_IFRAME_NETWORK", true))
-    <iframe src="http://{{$site->getHost()}}/ads" width="100%" style="border: none;height:305px;overflow:hidden !important;"></iframe>
+    <?php $agent = new \Jenssegers\Agent\Agent() ?>;
+
+    @if (!$agent->isMobile())
+        <section>
+            <iframe src="http://{{$site->getHost()}}/ads" width="100%" style="border: none;height:305px;overflow:hidden !important;"></iframe>
+        </section>
+    @endif
 @endif
