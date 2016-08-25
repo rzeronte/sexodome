6db9423b309202d977900a8e2a72aaf2943918ce
eduardo <eduardo.rzeronte@gmail.com>
2016-08-25 21:49:34 +0200

    change footer

--- resources/views/tube/commons/_footer.blade.php
+++ resources/views/tube/commons/_footer.blade.php
@@ -1,24 +1,20 @@
 <section class="footer">
     <div class="container">
-        <div class="col-md-4 text-left">
-            @if (env("PORN_STATIC_LINKS", false))
-                <a href="{{route('dmca', ["host" => $site->getHost()])}}">DMCA/Copyright</a> |
-                <a href="{{route('terms', ["host" => $site->getHost()])}}">Terms of use</a> |
-                <a href="{{route('C2257', ["host" => $site->getHost()])}}">2257</a> |
-                <a href="mailto:{{$site->contact_email}}">Contact</a>
-            @endif
-        </div>
-        <div class="col-md-4 text-center">
-            <a href="{{route('categories', ['profile' => $profile])}}" title="{{$language->title}}">
-                @if (file_exists(\App\rZeBot\rZeBotCommons::getLogosFolder()."/".md5($site->id).".png"))
-                    <img src="{{asset('/logos/'.md5($site->id).".png")}}" style="max-height: 50px;"/>
-                @else
-                    {{$site->domain}}
+        <div class="row">
+            <div class="col-md-4 col-sm-12 col-xs-12  text-left">
+                @if (env("PORN_STATIC_LINKS", false))
+                    <a href="{{route('dmca', ["host" => $site->getHost()])}}">DMCA/Copyright</a> |
+                    <a href="{{route('terms', ["host" => $site->getHost()])}}">Terms of use</a> |
+                    <a href="{{route('C2257', ["host" => $site->getHost()])}}">2257</a> |
+                    <a href="mailto:{{$site->contact_email}}">Contact</a>
                 @endif
-            </a>
-        </div>
-        <div class="col-md-4 text-right">
-            <a href="http://www.sexodome.com" target="_blank">made in by sexodome.com</a>
+            </div>
+            <div class="col-md-4 col-sm-12 col-xs-12  text-center">
+            </div>
+            <div class="col-md-4 col-sm-12 col-xs-12 text-right">
+                <a href="http://www.sexodome.com" target="_blank">made in by sexodome.com</a>
+            </div>
+
         </div>
     </div>
 </section>
