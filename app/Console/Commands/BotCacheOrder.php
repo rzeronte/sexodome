c1644c356651f4cb984a8ece3ac977e524add53a
eduardo <eduardo.rzeronte@gmail.com>
2016-09-04 22:13:47 +0200

    update cache order

--- app/Console/Commands/BotCacheOrder.php
+++ app/Console/Commands/BotCacheOrder.php
@@ -23,8 +23,13 @@ class BotCacheOrder extends Command
         $sites = Site::all();
 
         foreach($sites as $site) {
+            $pageViews = LaravelAnalyticsFacade::setSiteId('ga:'.$site->ga_account)->getMostVisitedPages(90, $maxResults = 100);
+            print_r($pageViews);
+            continue;
+
+            // ORDER SCENES
             DB::transaction(function () use ($site) {
-                rZeBotUtils::message("Generating CACHE ORDER for " . $site->getSitemap(), "green", true, true);
+                rZeBotUtils::message("Generating CACHE SCENES ORDER for " . $site->getHost(), "green", true, true);
                 $scenes = Scene::getScenesOrderBySceneClicks($site->id)->get();
 
                 $i = 1;
@@ -32,6 +37,24 @@ class BotCacheOrder extends Command
                     rZeBotUtils::message("Scene: " . $scene->id . ", Clicks: " . $scene->clicks . ", order: " . $i, "green", true, true);
                     $scene->cache_order = $i;
                     $scene->save();
+                    $i++;
+                }
+
+            });
+
+
+
+
+            // ORDER CATEGORIES
+            DB::transaction(function () use ($site) {
+                rZeBotUtils::message("Generating CACHE CATEGORIES ORDER for " . $site->getHost(), "green", true, true);
+                $categories = $site->categories()->get();
+
+                $i = 1;
+                foreach($categories as $category) {
+                    rZeBotUtils::message("Category: " . $category->id . ", order: " . $i, "green", true, true);
+                    $category->cache_order = $i;
+                    $category->save();
                     $i++;
                 }
 
