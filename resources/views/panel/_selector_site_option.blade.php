b90f2bfd08335d1140f925677b5f0694d19bc0a2
eduardo <eduardo.rzeronte@gmail.com>
2016-09-06 04:11:07 +0200

    minor fix

--- resources/views/panel/_selector_site_option.blade.php
+++ resources/views/panel/_selector_site_option.blade.php
@@ -1,6 +1,6 @@
 <div class='row'>
     <div class='col-md-12'>
-        <span><img src='{{asset("flags/".$site->language->code.".png")}}'/> {{$s->getHost()}}</span>
+        <span><img src='{{asset("flags/".$s->language->code.".png")}}'/> {{$s->getHost()}}</span>
         <small>
             <span class='label label-success'><i class='glyphicon glyphicon-film'></i> Scenes: {{$s->getTotalScenes()}}</span>
             <span class='label label-success'><i class='glyphicon glyphicon-th-large'></i> Categories: {{$s->categories()->where('status', 1)->count()}}</span>
