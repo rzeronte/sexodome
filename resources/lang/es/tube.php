a6afa66892a77ac6705e35c8c787878058e0c43c
eduardo <eduardo.rzeronte@gmail.com>
2016-09-12 01:43:49 +0200

    lang es

---
+++ resources/lang/es/tube.php
@@ -0,0 +1,18 @@
+<?php
+
+return [
+    'header_pornstars_btn' => 'Pornstars',
+    'header_inputsearch_placeholder' => 'que quieres ver?',
+    'header_inputsearch_search' => 'Buscar',
+
+    'h1_info_categories' => 'categorías',
+    'h1_info_porn_videos' => 'videos porno',
+
+    'count_porn_videos' => 'videos porno',
+    'count_porn_videos_for' => 'videos porno para',
+
+    'btn_order_news' => 'Nuevos videos porno',
+    'btn_order_mostpopular' => 'Videos porno mas populares',
+
+    'views' => 'visto'
+];
