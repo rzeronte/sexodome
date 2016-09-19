c5e4bbc5efcc7f8d0cb0b37901f1a94b5fdf318f
eduardo <eduardo.rzeronte@gmail.com>
2016-09-20 01:33:35 +0200

    number format

--- resources/views/tube/commons/_pornstars.blade.php
+++ resources/views/tube/commons/_pornstars.blade.php
@@ -1,7 +1,7 @@
 <div class="container">
     <div class="container header_title_section">
         <h2>
-            {{$pornstars->total()}} pornstars
+            {{number_format($pornstars->total(), 0, ".", ",")}} pornstars
         </h2>
     </div>
 
