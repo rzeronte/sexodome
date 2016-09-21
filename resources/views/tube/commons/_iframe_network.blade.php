85dbe3dd5ffa77278ec860d9538e2873a67309d7
eduardo <eduardo.rzeronte@gmail.com>
2016-09-22 00:54:41 +0200

    fix and color iframe

--- resources/views/tube/commons/_iframe_network.blade.php
+++ resources/views/tube/commons/_iframe_network.blade.php
@@ -5,7 +5,7 @@
         @if ($site->iframe_site_id != "")
             <?php $iframeSite = \App\Model\Site::find($site->iframe_site_id); ?>
             <div class="container">
-                <iframe src="http://{{$iframeSite->getHost()}}/ads?c={{$site->color}}" width="100%" style="border: none;height:305px;overflow:hidden !important;"></iframe>
+                <iframe src="http://{{$iframeSite->getHost()}}/ads?c={{str_replace('#', '', $site->color)}}" width="100%" style="border: none;height:305px;overflow:hidden !important;"></iframe>
             </div>
         @endif
     @endif
