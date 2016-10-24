0db5f537d84409822adce4a26c54366d36b45854
eduardo <eduardo.rzeronte@gmail.com>
2016-10-24 22:21:28 +0200

    404 redirect

---
+++ resources/views/errors/404.blade.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <title>Not found</title>
+</head>
+
+<body>
+<script type="text/javascript">
+    window.location = 'http://{{Request::server("HTTP_HOST")}}';
+</script>
+</body>
