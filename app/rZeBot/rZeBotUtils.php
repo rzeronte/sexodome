063ee8d4b9a3025f976c57f3d3030cb0c7e07bf1
eduardo <eduardo.rzeronte@gmail.com>
2016-12-13 20:42:14 +0100

    fix utils

--- app/rZeBot/rZeBotUtils.php
+++ app/rZeBot/rZeBotUtils.php
@@ -627,7 +627,7 @@ class rZeBotUtils
             curl_exec($ch);
 
             //$httpCode = curl_getinfo($ch, CURLINFO_HTTP_CODE);
-            rZeBotUtils::message("[$i DOWNLOADING THUMBNAIL] $src", "cyan", false, false);
+            rZeBotUtils::message("[$i DOWNLOADING THUMBNAIL] $src", "cyan", true, true);
 
             return true;
 
