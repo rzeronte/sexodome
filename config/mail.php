bfc36fdbd3ef0e1390e680115acfebfd9ff99588
darkhead <eduardo.rzeronte@gmail.com>
2017-01-26 01:27:45 +0100

    fix

--- config/mail.php
+++ config/mail.php
@@ -54,7 +54,7 @@ return [
     |
     */
 
-    'from' => ['address' => null, 'name' => null],
+    'from' => ['address' => "sexodomeweb@gmail.com", 'name' => "Sexodome - Porn Tube Generator"],
 
     /*
     |--------------------------------------------------------------------------
