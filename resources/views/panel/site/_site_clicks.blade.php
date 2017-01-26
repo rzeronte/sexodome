7dceca0cd12165b66ac51a5b6284285d2ff6b2f8
darkhead <eduardo.rzeronte@gmail.com>
2017-01-26 07:20:08 +0100

    eliminada gráfica uas

--- resources/views/panel/site/_site_clicks.blade.php
+++ resources/views/panel/site/_site_clicks.blade.php
@@ -6,15 +6,9 @@
         </div>
     </div>
 
-    <div class="row">
-        <div class="col-md-12">
-            <div id="graph_site_clicks_ua_{{$site->id}}" style="padding:5px;width:1024px; height:300px;border: solid 1px cornflowerblue;margin-top:10px;" class="text-center"></div>
-        </div>
-    </div>
-
     <script type="text/javascript">
         <?php $dataClicks = $site->getClicks($fi, $ff)->get(); ?>
-        <?php $dataUAs = $site->getUAs($fi, $ff)->get();?>
+        <?php //$dataUAs = $site->getUAs($fi, $ff)->get();?>
         <?php $rangeDates = \App\rZeBot\rZeBotUtils::date_range($fi, $ff, '+1 day', 'Y-m-d')?>
 
         <?php
@@ -59,44 +53,6 @@
             });
         });
 
-        $(function () {
-            $('#graph_site_clicks_ua_<?=$site->id?>').highcharts({
-                chart: {
-                    plotBackgroundColor: null,
-                    plotBorderWidth: null,
-                    plotShadow: false,
-                    type: 'pie'
-                },
-                title: {
-                    text: 'Clicks by User-Agent'
-                },
-                tooltip: {
-                    pointFormat: '{series.name}: <b>{point.percentage:.1f}%</b>'
-                },
-                plotOptions: {
-                    pie: {
-                        allowPointSelect: true,
-                        cursor: 'pointer',
-                        dataLabels: {
-                            enabled: true,
-                            format: '<b>{point.name}</b>: {point.percentage:.1f} %',
-                            style: {
-                                color: (Highcharts.theme && Highcharts.theme.contrastTextColor) || 'black'
-                            }
-                        }
-                    }
-                },
-                series: [{
-                    name: 'Brands',
-                    colorByPoint: true,
-                    data: [
-                        @foreach($dataUAs as $ua )
-                            { name:@if ($ua->name) '{{str_limit($ua->name, 25, $end = '...')}}' @else 'Undefined' @endif, y: {{$ua->y}} },
-                        @endforeach
-                    ]
-                }]
-            });
-        })
     </script>
 
 </div>
