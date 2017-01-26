421e292ac98e9e2f29b82b785a10319fcca53555
darkhead <eduardo.rzeronte@gmail.com>
2017-01-26 06:34:55 +0100

    category search

--- resources/views/panel/ajax/_ajax_category_thumbs.blade.php
+++ resources/views/panel/ajax/_ajax_category_thumbs.blade.php
@@ -1,7 +1,7 @@
-<div class="category-thumbs-{{$category->id}}" style="width:100%;">
+<div class="category-thumbs-{{$category->id}}" style="text-align:center; width:100%;">
     <?php $pos = 0 ?>
     @foreach ($scenes as $scene)
-        <img data-category-id="{{$category->id}}" data-thumb-number="{{$pos}}" class="category-thumb-image-selector" src="{{$scene->preview}}" style="width:100px;margin:14px;"/>
+        <img data-category-id="{{$category->id}}" data-thumb-number="{{$pos}}" class="category-thumb-image-selector" src="{{$scene->preview}}" style="width:150px;margin:15px;"/>
         <?php $pos++ ?>
     @endforeach
 </div>
