5e1cce8b04858af8b3d0282fee406dbe1f7ccfcc
darkhead <eduardo.rzeronte@gmail.com>
2017-03-05 21:49:59 +0100

    fix

--- app/Http/Controllers/ConfigController.php
+++ app/Http/Controllers/ConfigController.php
@@ -1208,6 +1208,8 @@ class ConfigController extends Controller
         }
 
         $site = Site::find($site_id);
+        
+        DB::table('categories')->where('site_id', $site->id)->update(['cache_order' => 0]);
 
         $categories = Category::getTranslationByStatus(1, $this->commons->language->id)
             ->where('site_id', '=', $site->id)
