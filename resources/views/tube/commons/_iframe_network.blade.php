3092c30011f29d6215121f77168e2ff7a9b9d4fc
darkhead <eduardo.rzeronte@gmail.com>
2017-03-06 02:08:50 +0100

    fix

--- resources/views/tube/commons/_iframe_network.blade.php
+++ resources/views/tube/commons/_iframe_network.blade.php
@@ -4,7 +4,7 @@
     @if ($site->iframe_site_id != "")
         <?php $iframeSite = \App\Model\Site::find($site->iframe_site_id); ?>
         <div class="container">
-            <iframe src="http://{{$iframeSite->getHost()}}/ads?c={{str_replace('#', '', $site->color)}}&c10={{str_replace('#', '', $site->color10)}}&c11={{str_replace('#', '', $site->color11)}}&c6={{str_replace('#', '', $site->color6)}}" width="100%" style="border: none;height:305px;overflow:hidden !important;"></iframe>
+            <iframe src="http://{{$iframeSite->getHost()}}/ads?c={{str_replace('#', '', $site->color)}}&c10={{str_replace('#', '', $site->color10)}}&c11={{str_replace('#', '', $site->color11)}}&c6={{str_replace('#', '', $site->color6)}}" width="100%" style="border: none;height:190px;overflow:hidden !important;"></iframe>
         </div>
     @endif
 @endif
