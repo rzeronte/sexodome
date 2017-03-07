9010ea01910f5d17067a17ecfb631926ca88632c
darkhead <eduardo.rzeronte@gmail.com>
2017-03-07 01:23:21 +0100

    mini slick

--- resources/views/tube/ads.blade.php
+++ resources/views/tube/ads.blade.php
@@ -7,7 +7,7 @@
     <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
 
-    <script src="{{asset('/slick/slick.min.js')}}" type="text/javascript"></script>
+    <script src="{{asset('/slick/slick.js')}}" type="text/javascript"></script>
     <link rel="stylesheet" href="{{asset('slick/slick.css')}}" />
     <link rel="stylesheet" href="{{asset('slick/slick-theme.css')}}" />
 
