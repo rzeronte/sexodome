c7958692bc3e4384c2d8aa4d1f97ef96c28030b3
darkhead <eduardo.rzeronte@gmail.com>
2017-04-11 00:41:56 +0200

    optimize load json with transaction

--- app/Console/Commands/BotLoadJSONCategories.php
+++ app/Console/Commands/BotLoadJSONCategories.php
@@ -12,7 +12,6 @@ use App\rZeBot\rZeBotUtils;
 use App\Model\Host;
 use App\Model\Tweet;
 use App\rZeBot\TwitterAPIExchange;
-use App\Model\Scene;
 use DB;
 use Illuminate\Support\Facades\Storage;
 
@@ -40,34 +39,37 @@ class BotLoadJSONCategories extends Command
         $categories = Storage::get('categories.json');
         $categories = \GuzzleHttp\json_decode($categories, true);
         
-        $i = 0;
         rZeBotUtils::message("Load categories for ". $site->getHost(), "cyan", false, false);
-        foreach ($categories as $c) {
-            $i++;
-            $category_en = $c['text_en'];
-            rZeBotUtils::message("Loading $category_en for ". $site->getHost(), "cyan", false, false);
 
-            if (Category::getTranslationByName(trim($category_en), 2, $site->id)->count() == 0) {
-                $newCategory = new Category();
-                $newCategory->status = 0;
-                $newCategory->site_id = $site_id;
-                $newCategory->text = $category_en;
-                $newCategory->nscenes = 0;
-                $newCategory->save();
+        DB::transaction(function () use ($categories, $site) {
+            $i = 0;
+            foreach ($categories as $c) {
+                $i++;
+                $category_en = $c['text_en'];
+                rZeBotUtils::message("Loading $category_en for ". $site->getHost(), "cyan", false, false);
 
-                foreach(Language::all() as $language) {
-                    if (isset($c['text_'.$language->code])) {
-                        $newCategoryTranslation = new CategoryTranslation();
-                        $newCategoryTranslation->category_id = $newCategory->id;
-                        $newCategoryTranslation->language_id = $language->id;
-                        $newCategoryTranslation->name = $c['text_'.$language->code];
-                        $newCategoryTranslation->permalink = str_slug($c['text_'.$language->code]);
-                        $newCategoryTranslation->save();
+                if (Category::getTranslationByName(trim($category_en), 2, $site->id)->count() == 0) {
+                    $newCategory = new Category();
+                    $newCategory->status = 0;
+                    $newCategory->site_id = $site->id;
+                    $newCategory->text = $category_en;
+                    $newCategory->nscenes = 0;
+                    $newCategory->save();
+
+                    foreach(Language::all() as $language) {
+                        if (isset($c['text_'.$language->code])) {
+                            $newCategoryTranslation = new CategoryTranslation();
+                            $newCategoryTranslation->category_id = $newCategory->id;
+                            $newCategoryTranslation->language_id = $language->id;
+                            $newCategoryTranslation->name = $c['text_'.$language->code];
+                            $newCategoryTranslation->permalink = str_slug($c['text_'.$language->code]);
+                            $newCategoryTranslation->save();
+                        }
                     }
+                } else {
+                    $this->error("$i) Categoría ya existe: " . $category_en);
                 }
-            } else {
-                $this->error("$i) Categoría ya existe: " . $category_en);
             }
-        }
+        });
     }
 }
