a8775cb4aeedf2df2edf17a7ab8240b2935f817a
darkhead <eduardo.rzeronte@gmail.com>
2017-04-15 23:31:43 +0200

    optimize

--- app/Model/Category.php
+++ app/Model/Category.php
@@ -51,6 +51,7 @@ class Category extends Model
 
         if ($order_by_nscenes !== false) {
             $categories->orderBy('categories.nscenes', 'DESC');
+            $categories->orderBy('categories.name', 'ASC');
         }
 
         return $categories;
