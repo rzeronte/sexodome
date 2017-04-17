e7845e1330f27b9b83714c266a54cb808ae3bf30
darkhead <eduardo.rzeronte@gmail.com>
2017-04-17 02:06:36 +0200

    minor fix iframe responsive

--- resources/views/tube/ads.blade.php
+++ resources/views/tube/ads.blade.php
@@ -126,26 +126,17 @@
             prevArrow: '<button type="button" class="slick-prev">Previous</button>',
             responsive: [
                 {
-                    breakpoint: 1024,
-                    settings: {
-                        slidesToShow: 3,
-                        slidesToScroll: 3,
-                        infinite: true,
-                        dots: true
-                    }
-                },
-                {
                     breakpoint: 600,
                     settings: {
-                        slidesToShow: 2,
-                        slidesToScroll: 2
+                        slidesToShow: 3,
+                        slidesToScroll: 3
                     }
                 },
                 {
                     breakpoint: 480,
                     settings: {
-                        slidesToShow: 1,
-                        slidesToScroll: 1
+                        slidesToShow: 3,
+                        slidesToScroll: 3
                     }
                 }
                 // You can unslick at a given breakpoint now by adding:
