cf461f2205326397d358396462a056a80d013374
darkhead <eduardo.rzeronte@gmail.com>
2017-04-17 02:04:52 +0200

    minor fix iframe responsive

--- resources/views/tube/ads.blade.php
+++ resources/views/tube/ads.blade.php
@@ -124,6 +124,34 @@
             slidesToScroll: 6,
             nextArrow: '<button type="button" class="slick-next">Next</button>',
             prevArrow: '<button type="button" class="slick-prev">Previous</button>',
+            responsive: [
+                {
+                    breakpoint: 1024,
+                    settings: {
+                        slidesToShow: 3,
+                        slidesToScroll: 3,
+                        infinite: true,
+                        dots: true
+                    }
+                },
+                {
+                    breakpoint: 600,
+                    settings: {
+                        slidesToShow: 2,
+                        slidesToScroll: 2
+                    }
+                },
+                {
+                    breakpoint: 480,
+                    settings: {
+                        slidesToShow: 1,
+                        slidesToScroll: 1
+                    }
+                }
+                // You can unslick at a given breakpoint now by adding:
+                // settings: "unslick"
+                // instead of a settings object
+            ]
         });
     });
 </script>
