6998f27505e8cbde86b8b5d62a50b9931a3f4ba7
darkhead <eduardo.rzeronte@gmail.com>
2017-04-21 02:41:03 +0200

    routes try fix

--- app/Console/Commands/BotScenesTaggerize.php
+++ app/Console/Commands/BotScenesTaggerize.php
@@ -46,28 +46,31 @@ class BotScenesTaggerize extends Command
         }
 
         $scenes = $site->scenes()->get();
-        foreach($scenes as $scene) {
-            $scene_title = $scene->translations('language_id', 2)->first()->title;
 
-            $palabras = explode(" ", $scene_title);
+        DB::transaction(function () use ($scenes, $site_id, $site) {
+            foreach($scenes as $scene) {
+                $scene_title = $scene->translations('language_id', 2)->first()->title;
 
-            $associated_tag_ids = [];
+                $palabras = explode(" ", $scene_title);
 
-            foreach ($palabras as $palabra) {
-                if (strlen($palabra) >= 2) {
-                    if (\App\Model\Tag::getTranslationSearch($palabra, 2, $site_id)->count() > 0) {
-                        foreach (\App\Model\Tag::getTranslationSearch($palabra, 2, $site_id)->orderBy(DB::raw('LENGTH(name)'))->limit(7)->get() as $tag) {
-                            $associated_tag_ids[] = $tag->id;
+                $associated_tag_ids = [];
+
+                foreach ($palabras as $palabra) {
+                    if (strlen($palabra) >= 4) {
+                        if (\App\Model\Tag::getTranslationSearch($palabra, 2, $site_id)->count() > 0) {
+                            foreach (\App\Model\Tag::getTranslationSearch($palabra, 2, $site_id)->orderBy(DB::raw('LENGTH(name)'))->limit(7)->get() as $tag) {
+                                $associated_tag_ids[] = $tag->id;
+                            }
                         }
                     }
                 }
+
+                $associated_tag_ids = array_unique($associated_tag_ids);
+                rZeBotUtils::message("Escena " . $scene->id . " asociada con " . count($associated_tag_ids) . " tags", "cyan", false, false);
+
+                $scene->tags()->sync($associated_tag_ids);
             }
-
-            $associated_tag_ids = array_unique($associated_tag_ids);
-            rZeBotUtils::message("Escena " . $scene->id . " asociada con " . count($associated_tag_ids) . " tags", "cyan", false, false);
-
-            $scene->tags()->sync($associated_tag_ids);
-        }
+        });
 
 
     }
