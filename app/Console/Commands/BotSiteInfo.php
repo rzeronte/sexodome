1ff42b808bd0698290f5b9abcba81f7a27e32bbe
darkhead <eduardo.rzeronte@gmail.com>
2017-04-22 02:11:35 +0200

    add info botsiteinfo

--- app/Console/Commands/BotSiteInfo.php
+++ app/Console/Commands/BotSiteInfo.php
@@ -27,6 +27,7 @@ class BotSiteInfo extends Command
     public function handle()
     {
         $domain = $this->argument("domain");
+
         $site = Site::where('domain', $domain)->first();
 
         if (!$site) {
@@ -34,13 +35,34 @@ class BotSiteInfo extends Command
             return;
         }
 
+        $categories_no_tags = 0;
+        $scene_no_tags = 0;
+
+        foreach ($site->scenes()->select('id')->get() as $scene) {
+            if ($scene->tags()->count() > 0) {
+                $scene_no_tags++;
+            }
+        }
+
+        foreach ($site->categories()->select('id')->get() as $category) {
+            if ($category->tags()->count() > 0) {
+                $categories_no_tags++;
+            }
+        }
+
         rZeBotUtils::message("About " . $site->getHost(), "green", true, true);
-        rZeBotUtils::message("Num. Scenes: " . $site->scenes()->count(), "green", true, true);
+
+        rZeBotUtils::message("Num. Scenes getTotalScenes(): " . $site->getTotalScenes(), "green", true, true);
+        rZeBotUtils::message("Num. Scenes No Tags: " . $scene_no_tags, "green", true, true);
+        rZeBotUtils::message("Num. Scenes BBDD: " . $site->scenes()->count(), "green", true, true);
         rZeBotUtils::message("Num. Categories: " . $site->categories()->count(), "green", true, true);
+        rZeBotUtils::message("Num. Categories No Tags: " . $categories_no_tags, "green", true, true);
+        rZeBotUtils::message("Num. Tags: " . $site->tags()->count(), "green", true, true);
         rZeBotUtils::message("Status: " . ($site->status == 1) ? "On": "Off", "green", true, true);
         rZeBotUtils::message("Title: " . str_replace("{domain}", $site->getHost(), $site->title_index), "green", true, true);
         rZeBotUtils::message("Description: " . str_replace("{domain}", $site->getHost(), $site->description_index), "green", true, true);
         rZeBotUtils::message("Analytics GA: " . $site->ga_account, "green", true, true);
+
     }
 
 }
