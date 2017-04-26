87d884821483908a65ffeb7d374cf80d6d4a3613
darkhead <eduardo.rzeronte@gmail.com>
2017-04-27 01:54:50 +0200

    fix menu mobile

--- resources/views/tube/commons/_javascripts.blade.php
+++ resources/views/tube/commons/_javascripts.blade.php
@@ -1,7 +1,6 @@
-<script src="{{asset('js/jquery-2.1.4.min.js')}}"></script>
 
-<!--Bootstrap JS-->
-<script src="{{asset('js/bootstrap.min.js')}}"></script>
+<!--Bootstrap JS--><script src="{{asset('js/bootstrap.min.js')}}"></script>
+<script src="{{asset('js/jquery-2.1.4.min.js')}}"></script>
 
 <script type="text/javascript" src="{{asset('js/front.js')}}"></script>
 
