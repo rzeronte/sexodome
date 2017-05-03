b9566a94c898c64ed7d7a19e64644736f9068bc3
root <root@33842653d6db>
2017-05-03 16:48:53 +0000

    better json load type

--- app/Console/Commands/BotLoadJSONCategories.php
+++ app/Console/Commands/BotLoadJSONCategories.php
@@ -17,13 +17,15 @@ use Illuminate\Support\Facades\Storage;
 
 class BotLoadJSONCategories extends Command
 {
-    protected $signature = 'zbot:categories:json {site_id}';
+    protected $signature = 'zbot:categories:json {site_id}
+                            {--gays=false : Filter for Gay categories}';
 
     protected $description = 'Load categories from json for one ste';
 
     public function handle()
     {
         $site_id = $this->argument('site_id');
+        $gays = $this->option('gays');
 
         $site = Site::find($site_id);
 
@@ -32,16 +34,33 @@ class BotLoadJSONCategories extends Command
             return;
         }
 
+        if ($gays !== 'false') {
+            $gays = true;
+        } else {
+            $gays = false;
+        }
+
         $categories = Storage::get('categories.json');
         $categories = json_decode($categories, true);
         
         rZeBotUtils::message("Load categories for ". $site->getHost(), "cyan", false, false);
 
-        DB::transaction(function () use ($categories, $site) {
+        DB::transaction(function () use ($categories, $site, $gays) {
             $i = 0;
             foreach ($categories as $c) {
                 $i++;
                 $category_en = $c['text_en'];
+
+                if ($gays == true && !str_contains(strtolower($category_en), 'gay')) {
+                    rZeBotUtils::message("Jump '$category_en'' for ". $site->getHost() . " -> Not Gay with --gays=true", "cyan", false, false);
+                    continue;
+                }
+
+                if ($gays == false && str_contains(strtolower($category_en), 'gay')) {
+                    rZeBotUtils::message("Jump '$category_en'' for ". $site->getHost() . " -> Gay", "cyan", false, false);
+                    continue;
+                }
+
                 rZeBotUtils::message("Loading $category_en for ". $site->getHost(), "cyan", false, false);
 
                 if (Category::getTranslationByName(trim($category_en), 2, $site->id)->count() == 0) {
