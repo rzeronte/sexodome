bf78944619a41151ea9abccc99fe4ae95b6c87c4
darkhead <eduardo.rzeronte@gmail.com>
2017-05-28 18:03:35 +0200

    fix category section seo

--- resources/views/tube/category.blade.php
+++ resources/views/tube/category.blade.php
@@ -1,5 +1,8 @@
 @extends('tube.layouts.app')
 
+@section('seo_title'){{ $sexodomeKernel->getSite()->getCategoryTitle($page) }}@endsection
+@section('seo_description'){{ $sexodomeKernel->getSite()->getCategoryDescription() }}@endsection
+
 @section('pagination_seo')
     @if (isset($scenes))
         @if ( $scenes->currentPage() > 1)
