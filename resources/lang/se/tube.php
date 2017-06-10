da6ef388f3f173b1742878f260390de59ecdc777
darkhead <eduardo.rzeronte@gmail.com>
2017-06-10 17:23:53 +0200

    lang se

---
+++ resources/lang/se/tube.php
@@ -0,0 +1,24 @@
+<?php
+
+return [
+    'header_pornstars_btn' => 'Porrstjärnor',
+    'header_inputsearch_placeholder' => 'Vill du se?',
+    'header_inputsearch_search' => 'Sök',
+
+    'h1_info_categories' => 'kategorier',
+    'h1_info_porn_videos' => 'porrfilmer',
+
+    'count_porn_videos' => 'porrfilmer',
+    'count_porn_videos_for' => 'porrfilmer till',
+
+    'btn_order_news' => 'Nya porrfilmer',
+    'btn_order_mostpopular' => 'Populära porrfilmer',
+
+    'views' => 'visto',
+    'all_categories' => 'Alla kategorier',
+
+    'related_scenes' => 'Relaterade scener',
+    'page' => 'sida',
+
+    "404_h2" => "Tyvärr finns inte den här sidan",
+];
