08ecf956eef3aa16ff01073b24da63052ffd0482
darkhead <eduardo.rzeronte@gmail.com>
2017-06-15 22:18:50 +0200

    update footer nofollow

--- resources/views/tube/commons/_footer.blade.php
+++ resources/views/tube/commons/_footer.blade.php
@@ -2,11 +2,11 @@
     <div class="container">
         <div class="col-lg-4 col-md-4 col-sm-4 col-xs-12 text-left">
             <p class="text-muted">
-                <a href="{{route('dmca', ["host" => App::make('sexodomeKernel')->getSite()->getHost()])}}">DMCA/Copyright</a> |
-                <a href="{{route('terms', ["host" => App::make('sexodomeKernel')->getSite()->getHost()])}}">Terms of use</a> |
-                <a href="{{route('C2257', ["host" => App::make('sexodomeKernel')->getSite()->getHost()])}}">2257</a> |
-                <a href="mailto:{{App::make('sexodomeKernel')->getSite()->contact_email}}">Contact</a> |
-                <a href="http://www.rtalabel.org" target="_blank"><i class="icon-logo_rta"></i></a></p>
+                <a rel=nofollow" href="{{route('dmca', ["host" => App::make('sexodomeKernel')->getSite()->getHost()])}}">DMCA/Copyright</a> |
+                <a rel=nofollow" href="{{route('terms', ["host" => App::make('sexodomeKernel')->getSite()->getHost()])}}">Terms of use</a> |
+                <a rel=nofollow" href="{{route('C2257', ["host" => App::make('sexodomeKernel')->getSite()->getHost()])}}">2257</a> |
+                <a rel=nofollow" href="mailto:{{App::make('sexodomeKernel')->getSite()->contact_email}}">Contact</a> |
+                <a rel=nofollow" href="http://www.rtalabel.org" target="_blank"><i class="icon-logo_rta"></i></a></p>
         </div>
         <div class="col-md-4" style="margin-top:12px;">
             <div id="share"></div>
